9d97504b5375e26fe39c03e1b0f93d0f18b6e398
Fernando <ramirez.fernando2003@gmail.com>
2022-04-21 23:10:48 -0500

    first

--- web/static/script.js
+++ web/static/script.js
@@ -4,10 +4,69 @@ window.addEventListener('load', function () {
 
 function colorClicked(color) {
     console.log("color clicked", color);
-    // hmm
+    // hmm    
+    // In PUT request body, do GET request and add 1
+
+    putColor(color);
+    //colorGET(color);
 }
 
-// AJAX
+function putColor(color) {
+    let link = `/api/clicks/${color}`;
+    
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // GET request and add 1
+            var response = this.responseText + getColor(color);
+            //console.log(response);
+            //console.log(xhttp.responseText);
+        }
+    };
+
+    xhttp.open("PUT", link, true);
+    xhttp.send();
+
+    updateClickLabels();
+}
+
+function getColor(color) {
+    let link = `/api/clicks/${color}`;
+    
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // GET request and add 1
+            let response = this.responseText;
+            //console.log(response);
+            x = xhttp.response;
+            return x;
+        }
+    };
+
+    xhttp.open("GET", link, true);
+    xhttp.send();
+
+}
+/*
+function getColor(color) {
+    let link = `/api/clicks/${color}`;
+    let xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(this.responseText);
+        }
+    };
+
+    xhttp.open("GET", link, true);
+    xhttp.send();
+
+    return response;
+}
+*/
+
+
 function updateClickLabels() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
@@ -21,3 +80,4 @@ function updateClickLabels() {
     xhttp.open("GET", "/api/clicks", true);
     xhttp.send();
 }
+
